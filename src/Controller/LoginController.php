eaf0b27c9328652fba3fec7e8495683c5d51dc04
hauw <hauw.dominique@gmail.com>
2018-12-11 15:49:21 +0100

    create login controler

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function login()
-    {
-        return $this->render('login/login.html.twig');
+    public function login(AuthenticationUtils $authenticationUtils)
+    {        
+        return $this->render('login/login.html.twig'
+        , array(
+            'email'=>$authenticationUtils->getLastUsername(), //Lastusername = loggin mail(et non username)
+            'error'=>$authenticationUtils->getLastAuthenticationError(),
+        ));
     }
+
 }
