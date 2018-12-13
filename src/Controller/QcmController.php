8decf6f738de10845f2f8c7a9f1beb1013b6dc55
hauw <hauw.dominique@gmail.com>
2018-12-13 16:18:41 +0100

    create debut integreate choice in formulaire

--- src/Controller/QcmController.php
+++ src/Controller/QcmController.php
@@ -7,6 +7,7 @@ use App\Form\QcmType;
 use App\Form\QuestionType;
 
 use App\Entity\Questions;
+use App\Entity\Choice;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,12 +22,31 @@ class QcmController extends AbstractController
     public function createQcm(Request $request, ObjectManager $manager)
     {
         $qcm = new Qcm();
-
+        // -------------------------------------
+        $question= new Questions();
+        $question->setQuestion('question');
+                // ->setExperience('experience');
+        $qcm->addQuestion($question);
+        // -------------------------------------
+        // dump($qcm);
         $form = $this->createForm(QcmType::class, $qcm);
-        
+                    //->createForm(QuestionType::class, $response);//A voir avec Robin si il faut le faire
         $form->handleRequest($request);
+
+// ====================================================================================
+        // partie choice response
+        $response = new Questions();//variable pour les champs questions
+        // -------------------------------------------------------------
+        $choice = new Choice();
+        $choice->setResponse('response')
+                ->setChoice('choice');
+        $response->addChoice($choice);
+
         
-        dump($qcm);
+        $formResp = $this->createForm(QuestionType::class, $response);
+        $formResp->handleRequest($request);
+        
+ // =====================================================================================
         
         if($form->isSubmitted() && $form->isValid()){
 
@@ -34,12 +54,16 @@ class QcmController extends AbstractController
                 $question->addQcm($qcm);
                 $manager->persist($question);
             }
+
+            // penser à faire un foreach pour les responses
+
             $manager->persist($qcm);
             $manager->flush();
         }
         
         return $this->render('teacher/createQcm.html.twig', [
             'form' => $form ->createView(),
+            'formResp' => $formResp ->createView(),
         ]);
     }
 
