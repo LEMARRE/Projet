bea79304eff783ddb50566434b7b25bae0b0f2d0
somon <ludosomon@free.fr>
2018-12-14 15:21:31 +0100

    design + refont

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -28,4 +28,21 @@ class StudentController extends AbstractController
             'avatar' => $avatar
             ]); 
     }
+
+     /**
+     * @Route("/student/{id}/profil", name="student_profil", requirements={"id"="\d+"})
+     * @IsGranted ("ROLE_STUDENT")
+     */
+    public function profil($id, UserService $userService)
+    {
+        $user = $userService->getById($id);
+        $id = $user->getId();
+        $avatar = $user->getAvatar();
+        return $this->render (
+            'student/studentProfil.html.twig',
+            ['user' => $userService->getById($id),
+            'avatar' => $avatar
+            ]); 
+    }
 }
+
